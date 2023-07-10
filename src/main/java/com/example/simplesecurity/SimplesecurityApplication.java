3be67da7b96dd218dc330f0223947645178e372e
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-07-10 13:41:47 +0200

    step1-basicControllers

---
+++ src/main/java/com/example/simplesecurity/SimplesecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplesecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplesecurity/SimplesecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
