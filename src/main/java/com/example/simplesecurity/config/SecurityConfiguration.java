c59af00505486c23c1b9b6e4238a7d01b4442d6f
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-07-10 13:49:00 +0200

    fix

--- src/main/java/com/example/simplesecurity/config/SecurityConfiguration.java
+++ src/main/java/com/example/simplesecurity/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.simplesecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -7,6 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfiguration {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(crsf -> crsf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
